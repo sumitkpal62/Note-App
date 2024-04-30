ea5950b06a6497ab1d80320d70248933c9669e5c
sumitkpal62 <sciencebuddy51@gmail.com>
2024-04-30 18:47:19 +0530

    Database connection to server established

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
 const PORT = 4000;
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/api/notes', async (req, res) => {
-  res.json({ message: 'Hello World' });
+  const notes = await prisma.note.findMany();
+  res.json(notes);
 });
 
 app.listen(PORT, () => {
